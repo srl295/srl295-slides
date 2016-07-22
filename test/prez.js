0b93d95fb118b18c44ec2ce14a3f5575a2d1eedd
Steven R. Loomis <srloomis@us.ibm.com>
2016-07-21 18:02:17 -0700

    cleanup

--- test/prez.js
+++
@@ -1,9 +0,0 @@
-
-describe('Prepare for today’s presentation',function() {
-	it('Should let me project VGA', function(done) {
-		done();
-	});
-	it('Rewrite prez in remark.js', function(done) {
-		done();
-	});
-});
